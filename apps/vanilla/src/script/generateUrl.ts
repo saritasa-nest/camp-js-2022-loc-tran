4ae5f87d0e431373154b0f7dba1876015a807dfc
LocTran-saritasa <loc.tran@saritasa.com>
2022-07-14 10:05:19 +0700

    User URLSearchParams for setting query parameters #JC19-352

--- apps/vanilla/src/script/generateUrl.ts
+++ apps/vanilla/src/script/generateUrl.ts
@@ -9,12 +9,14 @@ import { findInLocalStorage } from '../services/localStorage';
  */
 export function generateUrl(url: string, queries: Queries): string {
   let queryString = `${url}?`;
+  const searchParams = new URLSearchParams();
   queries.queryList.forEach(query => {
     let { value } = query;
     if (value === query.defaultValue) {
       value = findInLocalStorage(value, query.localStorageName);
     }
-    queryString += `${query.name}=${value}&`;
+    searchParams.append(query.name, value.toString());
   });
+  queryString += searchParams.toString();
   return queryString;
 }
