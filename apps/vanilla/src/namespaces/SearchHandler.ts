0e08652f9f2c902718ff63fbfcc87fc74c6548a4
Dao Duong <dao.duong@saritasa.com>
2022-07-18 09:30:19 +0700

    change get value #JC19-381

--- apps/vanilla/src/namespaces/SearchHandler.ts
+++ apps/vanilla/src/namespaces/SearchHandler.ts
@@ -7,16 +7,16 @@ export namespace SearchHandler {
 
   /** Handle searching functionality. */
   export function handleSearch(): void {
-    const searchText = document.querySelector<HTMLInputElement>('.search__input')?.value;
+    const searchText = document.querySelector<HTMLInputElement>('.search__input');
     assertNonNullish(searchText);
-    localStorage.setItem(SEARCH_QUERY, searchText);
+    localStorage.setItem(SEARCH_QUERY, searchText.value);
     const sortOption = localStorage.getItem(SORT_LS);
     assertNonNullish(sortOption);
     const params = new URLSearchParams({
       offset: DEFAULT_OFFSET,
       limit: LIMIT.toString(),
       ordering: sortOption,
-      search: searchText,
+      search: searchText.value,
     });
 
     updateTable(generateUrl(ANIME_ROUTE, params), FIRST_PAGE);
