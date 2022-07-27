b02b854b4138c3b58665abe9faccf3b0b125e4b3
LocTran-saritasa <loc.tran@saritasa.com>
2022-07-27 12:42:22 +0700

    Add select component #JC19-412

--- apps/angular/src/shared/shared.module.ts
+++ apps/angular/src/shared/shared.module.ts
@@ -3,21 +3,23 @@ import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
 
 import { PaginationComponent } from './components/pagination/pagination.component';
+import { SelectComponent } from './components/select/select.component';
 
 /** Shared module. */
 @NgModule({
-  declarations: [PaginationComponent],
+  declarations: [PaginationComponent, SelectComponent],
   imports: [
     CommonModule,
     MatInputModule,
     MatFormFieldModule,
     MatPaginatorModule,
+    MatSelectModule,
+    MatOptionModule,
   ],
-  exports: [
-    PaginationComponent,
-    CommonModule,
-  ],
+  exports: [PaginationComponent, SelectComponent, CommonModule],
 })
 export class SharedModule {}
