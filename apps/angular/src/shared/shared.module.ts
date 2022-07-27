3c112ac2a460c4b19050f8ce12c79e057855bd49
LocTran-saritasa <loc.tran@saritasa.com>
2022-07-27 13:22:24 +0700

    Change to material table #JC19-400

--- apps/angular/src/shared/shared.module.ts
+++ apps/angular/src/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
 
 /** Shared module. */
 @NgModule({
   declarations: [],
   imports: [CommonModule],
+  exports: [MatTableModule],
 })
 export class SharedModule { }
