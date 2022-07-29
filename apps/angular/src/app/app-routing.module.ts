09fd1a5a875df363223a1c70f9f342a7cc50cd6d
LocTran-saritasa <loc.tran@saritasa.com>
2022-07-29 09:11:51 +0700

    Add deploy configuration #JC19-412

--- apps/angular/src/app/app-routing.module.ts
+++ apps/angular/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -12,5 +13,6 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
 })
 export class AppRoutingModule {}
