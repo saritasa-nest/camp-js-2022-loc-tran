6dea449a21a7fcfa5e8e10906ef71c1fd67cce5e
LocTran-saritasa <loc.tran@saritasa.com>
2022-08-10 23:59:06 +0700

    Add navigate for guard #JC19-442

--- apps/angular/src/core/guards/is-logged-in.guard.ts
+++ apps/angular/src/core/guards/is-logged-in.guard.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 import { AuthService } from '../services/auth.service';
+import { NavigateService } from '../services/navigate.service';
+
+const LOGIN_ROUTE = '/login/';
 
 /** Check is user logged in or not. */
 @Injectable()
 export class CheckIsLoggedInGuard implements CanActivate {
-  public constructor(private readonly authService: AuthService) {}
+  public constructor(private readonly authService: AuthService, private readonly navigateService: NavigateService) {}
 
   /**
    * @inheritdoc
    */
   public canActivate(): Observable<boolean> | Promise<boolean> | boolean {
-    return this.authService.isLoggedIn();
+    return this.authService.isLoggedIn().pipe(tap(isLoggedIn => {
+      if (isLoggedIn === false) {
+        this.navigateService.navigate(LOGIN_ROUTE);
+      }
+    }));
   }
 }
--- apps/angular/src/core/guards/is-not-logged-in.guard.ts
+++ apps/angular/src/core/guards/is-not-logged-in.guard.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 import { AuthService } from '../services/auth.service';
+import { NavigateService } from '../services/navigate.service';
+
+const HOME_ROUTE = '/';
 
 /** Check is user logged in or not. */
 @Injectable()
 export class CheckIsNotLoggedInGuard implements CanActivate {
 
-  public constructor(private readonly authService: AuthService) {}
+  public constructor(private readonly authService: AuthService, private navigateService: NavigateService) {}
 
   /**
    * @inheritdoc
    */
   public canActivate(): Observable<boolean> | Promise<boolean> | boolean {
-    return this.authService.isNotLoggedIn();
+    return this.authService.isNotLoggedIn().pipe(tap(isNotLoggedIn => {
+      if (isNotLoggedIn === false) {
+        this.navigateService.navigate(HOME_ROUTE);
+      }
+    }));
   }
 }
