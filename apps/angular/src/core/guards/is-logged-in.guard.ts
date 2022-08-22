53ff045b732a0e807554bc40434ac5267094ca5a
LocTran <loc.tran@saritasa.com>
2022-08-22 10:21:38 +0700

    Merge changes from branch authen #JC19-442

--- apps/angular/src/core/guards/is-logged-in.guard.ts
+++ apps/angular/src/core/guards/is-logged-in.guard.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { Observable, tap } from 'rxjs';
 
 import { AuthService } from '../services/auth.service';
-import { NavigateService } from '../services/navigate.service';
 
 const LOGIN_ROUTE = '/login/';
 
@@ -14,24 +13,19 @@ const LOGIN_ROUTE = '/login/';
 export class CheckIsLoggedInGuard implements CanActivate {
   public constructor(
     private readonly authService: AuthService,
-    private readonly navigateService: NavigateService,
+    private readonly router: Router,
   ) {}
 
   /**
    * @inheritdoc
    */
-<<<<<<< HEAD
   public canActivate(): Observable<boolean> | Promise<boolean> | boolean {
     return this.authService.isLoggedIn().pipe(
       tap(isLoggedIn => {
         if (!isLoggedIn) {
-          this.navigateService.navigate(LOGIN_ROUTE);
+          this.router.navigate([LOGIN_ROUTE]);
         }
       }),
     );
-=======
-  public canActivate(): Observable<boolean> {
-    return this.authService.isLoggedIn();
->>>>>>> feature/JC19-432-authorization-angular
   }
 }
