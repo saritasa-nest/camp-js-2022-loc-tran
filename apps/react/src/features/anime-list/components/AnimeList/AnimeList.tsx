469f604865a343e1e61eeeed0fc8f63ed992a0aa
LocTran-saritasa <loc.tran@saritasa.com>
2022-08-29 12:50:23 +0700

    Add useInfiniteScroll hook #JC19-484

--- apps/react/src/features/anime-list/components/AnimeList/AnimeList.tsx
+++ apps/react/src/features/anime-list/components/AnimeList/AnimeList.tsx
@@ -5,6 +5,7 @@ import {
 } from '@js-camp/react/store/anime/selectors';
 import { useAppDispatch, useAppSelector } from '@js-camp/react/store/store';
 import { Skeleton } from '@mui/material';
+import { useInfiniteScroll } from '../../../../hooks/useInfiniteScroll';
 import { FC, memo, useEffect } from 'react';
 
 import { Anime } from '../Anime';
@@ -12,6 +13,7 @@ import { Anime } from '../Anime';
 import styles from './AnimeList.module.css';
 
 const AnimeListComponent: FC = () => {
+  const { setLastElement } = useInfiniteScroll(() => console.log('Reached!'))
   const dispatch = useAppDispatch();
   const animeList = useAppSelector(selectAnimeList);
   const isLoading = useAppSelector(selectIsAnimeListLoading);
@@ -20,17 +22,22 @@ const AnimeListComponent: FC = () => {
     dispatch(fetchAnime());
   }, [dispatch, fetchAnime]);
 
-  if (isLoading) {
-    return <div className={styles['skeleton']}>
-      <Skeleton variant="rectangular" width={100} height={100} />
-      <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
-      <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
-    </div>;
-  }
+  const list = animeList.map((anime, index) => {
+    if (index === animeList.length - 1) {
+      return <div key={anime.id} ref={setLastElement}>
+        <Anime anime={anime} />
+      </div>;
+    }
+    return <Anime anime={anime} key={anime.id} />;
+  });
 
-  const list = animeList.map(anime => <Anime anime={anime} key={anime.id} />);
+  const skeleton = <div className={styles['skeleton']}>
+    <Skeleton variant="rectangular" width={100} height={100} />
+    <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+    <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+  </div>;
 
-  return <>{list}</>;
+  return <>{list}{skeleton}</>;
 };
 
 export const AnimeList = memo(AnimeListComponent);
---
+++ apps/react/src/hooks/useInfiniteScroll.ts
@@ -0,0 +1,31 @@
+import { useEffect, useRef, useState } from 'react';
+
+export const useInfiniteScroll = (action: () => void) => {
+  const [lastElement, setLastElement] = useState<HTMLElement | null>(null);
+  const observer = useRef(
+    new IntersectionObserver(
+      entries => {
+            const first = entries[0];
+            if (first.isIntersecting) {
+                action();
+            }
+        },
+    ),
+  );
+  useEffect(() => {
+    const currentElement = lastElement;
+    const currentObserver = observer.current;
+
+    if (currentElement) {
+        currentObserver.observe(currentElement);
+    }
+
+    return () => {
+        if (currentElement) {
+            currentObserver.unobserve(currentElement);
+        }
+    };
+  }, [lastElement]);
+
+  return { setLastElement };
+};
