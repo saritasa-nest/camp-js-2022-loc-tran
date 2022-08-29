84be8f00c4a84238a5b183683acdf64f7f3491b1
LocTran-saritasa <loc.tran@saritasa.com>
2022-08-29 11:16:12 +0700

    Add skeleton for anime list #JC19-474

--- apps/react/src/features/anime-list/components/AnimeList/AnimeList.tsx
+++ apps/react/src/features/anime-list/components/AnimeList/AnimeList.tsx
@@ -1,12 +1,15 @@
 import { fetchAnime } from '@js-camp/react/store/anime/dispatchers';
 import {
   selectAnimeList,
-  selectIsAnimeListLoading
+  selectIsAnimeListLoading,
 } from '@js-camp/react/store/anime/selectors';
 import { useAppDispatch, useAppSelector } from '@js-camp/react/store/store';
+import { Skeleton } from '@mui/material';
 import { FC, memo, useEffect } from 'react';
 
 import { Anime } from '../Anime';
+
+import styles from './AnimeList.module.css';
 
 const AnimeListComponent: FC = () => {
   const dispatch = useAppDispatch();
@@ -16,6 +19,14 @@ const AnimeListComponent: FC = () => {
   useEffect(() => {
     dispatch(fetchAnime());
   }, [dispatch, fetchAnime]);
+
+  if (isLoading) {
+    return <div className={styles['skeleton']}>
+      <Skeleton variant="rectangular" width={100} height={100} />
+      <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+      <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+    </div>;
+  }
 
   const list = animeList.map(anime => <Anime anime={anime} key={anime.id} />);
 
