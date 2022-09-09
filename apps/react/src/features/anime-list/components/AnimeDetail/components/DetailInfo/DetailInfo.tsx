d537ba09a2d7250361833c70b9903d5e315de37c
LocTran <loc.tran@saritasa.com>
2022-09-09 15:55:59 +0700

    Fix PR review #JC19-494

--- apps/react/src/features/anime-list/components/AnimeDetail/components/DetailInfo/DetailInfo.tsx
+++ apps/react/src/features/anime-list/components/AnimeDetail/components/DetailInfo/DetailInfo.tsx
@@ -20,52 +20,52 @@ const DetailInfoComponent: FC<Props> = ({ detail }) => <>
         {getPlaceholder(detail?.titleEnglish)}
       </span>
     </div>
-    <label className={styles['detail__label']}>
+    <div className={styles['detail__label']}>
       <span className={styles['detail__title']}>Japanese title: </span>
       <span className={styles['detail__text']}>
         {getPlaceholder(detail?.titleJapanese)}
       </span>
-    </label>
-    <label className={styles['detail__label']}>
+    </div>
+    <div className={styles['detail__label']}>
       <span className={styles['detail__title']}>Type: </span>
       <span className={styles['detail__text']}>{detail?.type}</span>
-    </label>
-    <label className={styles['detail__label']}>
+    </div>
+    <div className={styles['detail__label']}>
       <span className={styles['detail__title']}>Status: </span>
       <span className={styles['detail__text']}>{detail?.status}</span>
-    </label>
-    <label className={styles['detail__label']}>
+    </div>
+    <div className={styles['detail__label']}>
       <span className={styles['detail__title']}>Airing: </span>
       <span className={styles['detail__text']}>
         {detail?.airing ? 'Yes' : 'No'}
       </span>
-    </label>
-    <label className={styles['detail__label']}>
+    </div>
+    <div className={styles['detail__label']}>
       <span className={styles['detail__title']}>Aired start date: </span>
       <span className={styles['detail__text']}>
         {detail?.aired?.start.toLocaleString()}
       </span>
-    </label>
-    <label className={styles['detail__label']}>
+    </div>
+    <div className={styles['detail__label']}>
       <span className={styles['detail__title']}>Aired end date: </span>
       <span className={styles['detail__text']}>
         {detail?.aired?.end.toLocaleString()}
       </span>
-    </label>
-    <label className={styles['detail__label']}>
+    </div>
+    <div className={styles['detail__label']}>
       <span className={styles['detail__title']}>Synopsis: </span>
       <span className={styles['detail__text']}>
         {detail?.synopsis}
       </span>
-    </label>
-    <label className={styles['detail__label']}>
+    </div>
+    <div className={styles['detail__label']}>
       <span className={styles['detail__title']}>Genres: </span>
       <span className={styles['detail__text']}>
         {detail?.genresData.map(genre => (
           <Chip key={genre.id} color='primary' label={genre.name} />
         ))}
       </span>
-    </label>
+    </div>
   </div>
 </>;
 
